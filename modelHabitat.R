9f707d80df3042e350aa3e9bff6f0a4e3cd781e0
Sarah Chisholm <89938738+sarah-chisholm@users.noreply.github.com>
2023-02-27 22:19:59 +0000

    Load necessary datasheets, define parameters, draft structure of parameter sampling table

--- modelHabitat.R
+++ modelHabitat.R
@@ -20,17 +20,66 @@ progressBar(type = "message", message = "Preparing inputs...")
 myScenario <- scenario()
 
 # Load relevant datasheets
-RunControl <- datasheet(myScenario, "stsim_RunControl") # 
+# stsim
+RunControl <- datasheet(myScenario, "stsim_RunControl")
+Stratum <- datasheet(myScenario, "stsim_Stratum")
+SecondaryStrtaum <- datasheet(myScenario, "stsim_SecondaryStratum")
+StateClass <- datasheet(myScenario, "stsim_StateClass")
+InitialConditionsSpatial <- datasheet(myScenario, "stsim_InitialConditionsSpatial")
+OutputSpatialState <- datasheet(myScenario, "stsim_OutputSpatialState")
 
+#stsimsf
+OutputSpatialStockGroup <- datasheet(myScenario, "stsimsf_OutputSpatialStockGroup")
+StockType <- datasheet(myScenario, "stsimsf_StockType")
+StockGroup <- datasheet(myScenario, "stsimsf_StockGroup")
+StockGroupMembership <- datasheet(myScenario, "stsimsf_StockGroupMembership")
 
+# stsimNetweb
+OutputOptions <- datasheet(myScenario, "stsimNestweb_OutputOptions")
+OutputOptionsSpatial <- datasheet(myScenario, "stsimNestweb_OutputOptionsSpatial")
+HabitatModel <- datasheet(myScenario, "stsimNestweb_HabitatModel")
 
 ## Setup Parameters ----
-# zzz: read cell size and total area from raster
-# Cell size (90m x 90m) to Ha conversion
-scaleFactor <- 0.81
+# Iterations 
+iterations <- seq(RunControl$MinimumIteration, RunControl$MaximumIteration)
 
-# Total area (Ha) of analysis area
-totalArea <- 336475.6200
+# Timesteps
+timestepsTabular <- if(OutputOptions$SummaryOutputHA){
+  seq(RunControl$MinimumTimestep, RunControl$MaximumTimestep, by = OutputOptions$SummaryOutputHATimesteps) %>% 
+    c(RunControl$MaximumTimestep) %>% 
+    unique()
+} else c()
+
+timestepsSpatial <- if(OutputOptionsSpatial$RasterOutputHA){
+  seq(RunControl$MinimumTimestep, RunControl$MaximumTimestep, by = OutputOptionsSpatial$RasterOutputHATimesteps) %>% 
+    c(RunControl$MaximumTimestep) %>% 
+    unique()
+} else c()
+
+timesteps <- c(timestepsTabular, timestepsSpatial) %>% 
+  unique() %>% 
+  sort()
+
+# Square meter to hectare conversion
+scaleFactor <- 0.0001
+
+# Load template raster
+templateRaster <- rast(InitialConditionsSpatial$StratumFileName)
+templateRaster[!is.na(templateRaster)] <- 1
+
+# Pixel resolution
+cellResolution <- templateRaster %>% res()
+
+# Pixel count
+cellCount <- freq(templateRaster, value = 1)$count
+
+# Area of a pixel (square meter)
+cellArea <- cellResolution[1]^2
+
+# Total area
+totalArea <- cellCount * cellArea
+
+
 
 ## Setup files and folders ----
 
@@ -45,6 +94,38 @@ tempDir <- ssimEnvironment()$TempDirectory
 
 # Main Code Here zzz ----
 progressBar(type = "message", message = "Running main code...")
+
+# Build parameter sampling table
+models <- map_chr(HabitatModel$ModelFileName, load) %>% 
+  map(get) %>% 
+  set_names(HabitatModel$Name)
+
+# enframe(models, name = "Species", value = "Model") %>% 
+#   mutate()
+
+# imap_dfr(
+#   models, 
+#   ~{
+#     tibble(
+#       Species = .y,
+#       Parameter = c("Aspen Cover", "Diameter"),
+#       Mean = c(0),
+#       SD = c(0)
+#     )
+#     })
+
+# piwo_mods$coefArray %>% as_tibble
+
+for(iteration in iterations){
+  # Sample parameters
+  for(timestep in timesteps){
+    # Get relevant spatial data
+    for(aSpecies in species){
+      # Predict and accumulate outputs
+      
+    }
+  }
+}
 
 # Clean up ----
 
