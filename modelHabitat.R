ae1ca328131f28d3e6a7bd19b4785edd7906644e
Sarah Chisholm <89938738+sarah-chisholm@users.noreply.github.com>
2023-03-01 20:00:42 +0000

    Update modelHabitat.R

--- modelHabitat.R
+++ modelHabitat.R
@@ -1,3 +1,8 @@
+# zzz:
+# Load spatial data to disambiguating habitat suitability (strata, site)
+# Create a datasheet for Site
+# Build habitat suitability maps
+
 # Set environment variable TZ when running on AWS EC2 instance
 Sys.setenv(TZ='UTC')
 
@@ -32,12 +37,14 @@ OutputSpatialState <- datasheet(myScenario, "stsim_OutputSpatialState")
 OutputSpatialStockGroup <- datasheet(myScenario, "stsimsf_OutputSpatialStockGroup")
 StockType <- datasheet(myScenario, "stsimsf_StockType")
 StockGroup <- datasheet(myScenario, "stsimsf_StockGroup")
-StockGroupMembership <- datasheet(myScenario, "stsimsf_StockGroupMembership")
+StockTypeGroupMembership <- datasheet(myScenario, "stsimsf_StockTypeGroupMembership")
 
 # stsimNetweb
 OutputOptions <- datasheet(myScenario, "stsimNestweb_OutputOptions")
 OutputOptionsSpatial <- datasheet(myScenario, "stsimNestweb_OutputOptionsSpatial")
 HabitatModel <- datasheet(myScenario, "stsimNestweb_HabitatModel")
+SpeciesID <- datasheet(myScenario, "stsimNestweb_Species", includeKey = TRUE) %>% 
+  pull(SpeciesID, name = Name)
 
 ## Setup Parameters ----
 # Iterations 
@@ -60,12 +67,16 @@ timesteps <- c(timestepsTabular, timestepsSpatial) %>%
   unique() %>% 
   sort()
 
+# Species
+species <- HabitatModel$Name
+
 # Square meter to hectare conversion
 scaleFactor <- 0.0001
 
 # Load template raster
 templateRaster <- rast(InitialConditionsSpatial$StratumFileName)
 templateRaster[!is.na(templateRaster)] <- 1
+names(templateRaster) <- "habitatSuitability"
 
 # Pixel resolution
 cellResolution <- templateRaster %>% res()
@@ -96,36 +107,86 @@ tempDir <- ssimEnvironment()$TempDirectory
 progressBar(type = "message", message = "Running main code...")
 
 # Build parameter sampling table
-models <- map_chr(HabitatModel$ModelFileName, load) %>% 
+modelNames <- map_chr(HabitatModel$ModelFileName, load)
+for(m in HabitatModel$ModelFileName) load(m)
+models <- modelNames %>% 
   map(get) %>% 
   set_names(HabitatModel$Name)
+rm(list = modelNames)
 
-# enframe(models, name = "Species", value = "Model") %>% 
-#   mutate()
+# Parameterize the sampling distribution for each parameter and model
+parameterTable <- imap_dfr(
+  models,
+  ~{
+    # Connect clean parameter names to variable names in model fit
+    parameters <- c('Aspen Cover' = 'scale(Perc_At)',
+                    'Diameter' = 'scale(Median_DBH)')
+    means <- coef(.x)[parameters]
+    stdErrors <- .x$coefArray[, 'Std. Error', parameters] %>% 
+      apply(MARGIN = 2, FUN = mean, na.rm = TRUE) # MARGIN = 2 is used to average within columns 
+    
+    tibble(
+      species = .y,
+      parameter = names(parameters),
+      mean = means,
+      sd = stdErrors)}) # Double check this assumption
 
-# imap_dfr(
-#   models, 
-#   ~{
-#     tibble(
-#       Species = .y,
-#       Parameter = c("Aspen Cover", "Diameter"),
-#       Mean = c(0),
-#       SD = c(0)
-#     )
-#     })
 
-# piwo_mods$coefArray %>% as_tibble
 
 for(iteration in iterations){
   # Sample parameters
+  parameterTable <- parameterTable %>% 
+    mutate(value = map2_dbl(mean, sd, rnorm, n = 1))
+  
   for(timestep in timesteps){
-    # Get relevant spatial data
+    # Load aspen cover and diameter
+    aspenCover <- datasheetRaster(
+      ssimObject = myScenario, 
+      datasheet = "stsimsf_OutputSpatialStockGroup", 
+      iteration = iteration, 
+      timestep = timestep,
+      filterColumn = "StockGroupID",
+      filterValue = "Aspen Cover (%) [Type]")
+    
+    # Convert aspen raster to proportion (rather than percentage)
+    aspenCover[] <- aspenCover[]/100
+    
+    diameter <- datasheetRaster(
+      ssimObject = myScenario, 
+      datasheet = "stsimsf_OutputSpatialStockGroup", 
+      iteration = iteration, 
+      timestep = timestep,
+      filterColumn = "StockGroupID",
+      filterValue = "Diameter (cm) [Type]")
+    
+    # Create dataframe of habitat suitability model inputs
+    habitatSuitabilityDf <- data.frame(Perc_At = aspenCover[], 
+                                       Median_DBH = diameter[],
+                                       edge_near = 0,
+                                       Num_2BI = 0,
+                                       Mean_decay = 0,
+                                       dist_to_cut = 0,
+                                       # zzz: how to handle categorical variables??
+                                       cut_harvest0 = "N",
+                                       Site = "YY")
+    
     for(aSpecies in species){
-      # Predict and accumulate outputs
+      # Predict habitat suitability
+      model <- models[[aSpecies]]
+      habitatSuitabilityDf$pred <- predict(model, newdata = habitatSuitabilityDf, type = "response", allow.new.levels = TRUE)
+      
+      # Output raster
+      outputFilename <- file.path(tempDir, str_c("hs.sp_", SpeciesID[aSpecies], ".it", iteration, ".ts", timestep, ".tif")) %>% 
+        normalizePath(mustWork = FALSE)
+      
+      rast(templateRaster, vals = habitatSuitabilityDf$pred) %>% 
+        writeRaster(outputFilename, overwrite = TRUE)
       
     }
   }
 }
+
+OutputSpatialHabitat <- tibble(FileName = list.files(tempDir, ".tif", full.names = TRUE)) #%>% 
 
 # Clean up ----
 
