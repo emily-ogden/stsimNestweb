74468c6bca40c488b46d46be02b9b5c3c49ad0a1
Sarah Chisholm <89938738+sarah-chisholm@users.noreply.github.com>
2023-03-10 22:50:57 +0000

    Calculate average habitat across iterations

--- summarizeHabitat.R
+++ summarizeHabitat.R
@@ -17,6 +17,12 @@ suppressPackageStartupMessages(library(terra))
 # Setup ----
 progressBar(type = "message", message = "Preparing inputs...")
 
+## Function definitions ----
+# Define function to facilitate recoding a vector using a look-up table
+lookup <- function(x, old, new){
+  dplyr::recode(x, !!!set_names(new, old))
+}
+
 ## Connect to SyncroSim ----
 myScenario <- scenario()
 
@@ -35,6 +41,7 @@ timesteps <- OutputSpatialHabitat$Timestep %>%
 # Species
 species <- OutputSpatialHabitat$Species %>% 
   unique() %>% 
+  as.character() %>% 
   sort()
 
 ## Setup files and folders ----
@@ -51,21 +58,45 @@ dir.create(spatialOutputDir)
 # Main Code Here ----
 if(OutputOptionsSpatial$RasterOutputHAAverage) {
   progressBar(type = "message", message = "Running main code...")
-  progressBar(type = "begin", totalSteps = length(iterations) * length(timesteps) * length(species))
+  progressBar(type = "begin", totalSteps = length(timesteps) * length(species))
   
   for(timestep in timesteps){
+    # Get all habitat suitability maps for a given timestep
+    habitatSuitability <- datasheetRaster(
+      ssimObject = myScenario, 
+      datasheet = "stsimNestweb_OutputSpatialHabitat", 
+      timestep = timestep) %>% 
+      rast()
+    
     for(aSpecies in species){
+      # Determine output filename based on species and tiemstep
       outputFilename <- file.path(spatialOutputDir, str_c("hsa.sp", SpeciesID[aSpecies], ".ts", timestep, ".tif")) %>% 
         normalizePath(mustWork = FALSE)
       
-      habitatSuitability <- datasheetRaster(
-        ssimObject = myScenario, 
-        datasheet = "stsimNestweb_OutputSpatialHabitat", 
-        timestep = timestep,
-        #iteration = 1,
-        filterColumn = "Species",
-        filterValue = aSpecies)
+      # Subset layers by species
+      layerNames <- names(habitatSuitability) %>% 
+        str_subset(str_c("sp", SpeciesID[aSpecies], "\\."))
+      
+      habitatSuitability[[layerNames]] %>% 
+        mean() %>% 
+        writeRaster(outputFilename, 
+                    overwrite = TRUE,
+                    NAflag = -9999)
+      # Increment 
+      progressBar()
     }
   }
-
+  OutputSpatialAverageHabitat <- data.frame(
+    FileName = list.files(spatialOutputDir, pattern = ".tif", full.names = TRUE) %>% 
+      normalizePath(),
+    Iteration = 1) %>% 
+    mutate(
+      temp = basename(FileName),
+      Timestep = temp %>% str_extract("ts\\d+") %>% str_replace("ts", "") %>% as.numeric(),
+      Species = temp %>% str_extract("sp\\d+") %>% str_replace("sp", "") %>% as.numeric(),
+      Species = lookup(Species, SpeciesID, names(SpeciesID))) %>% 
+    dplyr::select(-temp) %>% 
+    as.data.frame()
+  
+  saveDatasheet(myScenario, OutputSpatialAverageHabitat, "stsimNestweb_OutputSpatialAverageHabitat")
 }
